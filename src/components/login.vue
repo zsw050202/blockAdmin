a795bc6bab64ea16a5f15fc18ff9745245d5563f
zsw <1050259219@qq.com>
2018-05-16 17:28:04 +0800

    update

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,11 +1,53 @@
 <template>
 	<div class="login-wrapper">
-		
+		<div class="form">
+			<h2 class="title">zswBloc后台管理登录</h2>
+			<!-- 登陆表单 -->
+			<el-form ref="form">
+				<el-form-item>
+					<el-input v-model="form.username" placeholder="请输入管理员用户名"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-input v-model="form.userpwd" placeholder="请输入管理员密码"></el-input>
+				</el-form-item>
+			</el-form>
+		</div>
 	</div>
 </template>
 
 <script>
-	export default {};
+	export default {
+		data () {
+			return {
+				form: {
+					username: ''
+				}
+			};
+		}
+	};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+	.login-wrapper {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: #aaa;
+		text-align: center;
+		.form {
+			position: absolute;
+			top: 30%;
+			left: 50%;
+			margin-left: -15%;
+			width: 30%;
+			background: #fff;
+			padding: 20px;
+			border-radius: 5px;
+			.title {
+				margin-bottom: 20px;
+			}
+		}
+	}
+</style>
